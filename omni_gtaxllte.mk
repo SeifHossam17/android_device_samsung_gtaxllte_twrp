da07cb9c3252361faa05eac4b231eae43b974d4d
SeifHossam17 <102517370+SeifHossam17@users.noreply.github.com>
2023-09-28 11:14:15 +0300

    Update omni_gtaxllte.mk

--- omni_gtaxllte.mk
+++ omni_gtaxllte.mk
@@ -1,12 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
